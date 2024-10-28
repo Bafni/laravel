7bcd09aed9ea4d17386af82c9fd6499a6c9696e6
Bafni <reclamabafni@gmail.com>
2024-10-29 01:35:37 +0200

    first commit

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+        $this->map();
+    }
+
+    public function map()
+    {
+        $this->mapWebRoutes();
+        $this->mapApiRoutes();
+    }
+
+    /**
+     *
+     * @return void
+     */
+    private function mapApiRoutes(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(function () {
+                Route::prefix('{version}')
+                    ->group(function () {
+                        $version = request()->segment(2);
+                        $routeFile = base_path("routes/{$version}/api.php");
+                        if (file_exists($routeFile)) {
+                            require $routeFile;
+                        }
+                    });
+            });
+    }
+
+    protected
+    function mapWebRoutes(): void
+    {
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
+}
